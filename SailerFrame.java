3fab35416e09bb733386ceedbcc7cd7267254542
VeronaGH <KA117@mail.ru>
2017-05-03 12:08:00 +0300

    Update SailerFrame.java

--- SailerFrame.java
+++ SailerFrame.java
@@ -1,22 +1,5 @@
 package frame;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-
-import dao.DaoOrders;
-import dao.DaoProducts;
-import db.DB;
-import entity.Orders;
-import entity.Products;
-
 public class SailerFrame extends JFrame {
 
 	private JPanel panel;
